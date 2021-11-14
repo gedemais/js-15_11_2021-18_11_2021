bed4171c47fa1b34aaf02c83b749336f1c836d1f
jbirdqiqat <MbDBt.BN9hp@protonmail.com>
2021-11-14 04:59:36 +0100

    moving right along, still have to import stuff

--- examples/linking/script.js
+++ examples/linking/script.js
@@ -1,16 +1,22 @@
+function writeInput(formulaire)
+{
 /* On commence par recuperer l'objet de type 'paragraphe' dans
  * lequel on souhaite ecrire, en renseignant son id en parametre
  * de la methode getElementById de l'objet document.*/
 
-let paragraph = document.getElementById('texte')
+	let paragraph = document.getElementById('texte');
+
+// On recupere le texte ecrit dans le formulaire
+	input = document.formulaire.input.value;
 
 /* On peut maintenant reecrire le contenu du paragraphe,
  * grace a la methode innerText de l'element paragraphe.*/
 
-paragraph.innerText = `Hello World`
+	paragraph.innerText = input;
 
 /* Le but de cet exercice est de montrer comment integrer
  * un script js dans une page html, et de montrer que les
  * elements de la page en question peuvent etre modifies
  * par le script, pour donner un cas concret d'utilisation
  * du js.*/
+}
--- exercises/exercice_1/answer.js
+++
@@ -1,3 +0,0 @@
-
-/* Le but de cet exercice est de faire decouvrir les variables en JavaScript,
- * ainsi que la notion de scope avec "var" et "let".*/
