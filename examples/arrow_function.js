9010e5c17b83976de1c0e1cdcb9bd8248b48e667
jbirdqiqat <MbDBt.BN9hp@protonmail.com>
2021-11-15 22:18:58 +0100

    lots of exercises :)

---
+++ examples/arrow_function.js
@@ -0,0 +1,14 @@
+/* Les fonctions flechees permettent de raccourcir la syntaxe de declaration d'une
+ * fonction dans certains cas. Ces fonctions sont souvent anonymes, et ne devraient
+ * pas etre utilisees pour declarer des methodes dans un souci de clarte.*/
+
+// Une fonction classique :
+function square(n)
+{
+	return (n * n);
+}
+
+// La meme fonction, version flechee :
+square2 = n => n * n;
+
+console.log(square(4), square2(4));
---
+++ exercises/recursion.js
@@ -0,0 +1,15 @@
+function func(n)
+{
+	if (n == 64)
+	{
+		return (n);
+	}
+	else
+	{
+		return ();
+	}
+}
+
+n = func(0);
+
+console.assert(n == 64);
