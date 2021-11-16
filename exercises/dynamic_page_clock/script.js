1e4f2c013c16bc82035ff5b709915246758691e5
jbirdqiqat <MbDBt.BN9hp@protonmail.com>
2021-11-16 10:07:10 +0100

    phour handicap added

--- exercises/dynamic_page_clock/script.js
+++ exercises/dynamic_page_clock/script.js
@@ -3,6 +3,7 @@ clock  = new Clock(0, 0, 0);
 
 setInterval(async function tick()
 {
+	let	pHour = getElementById('heure'); // Cette ligne ne doit pas etre modifiee.
 
 }, 1000);
 /* setInterval est la pour permettre au script de s'executer toutes les 1000ms
