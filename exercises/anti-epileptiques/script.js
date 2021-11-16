b6309bbaead23fa6d13d30955df5244b8d421785
jbirdqiqat <MbDBt.BN9hp@protonmail.com>
2021-11-16 13:34:28 +0100

    new exercice

---
+++ exercises/anti-epileptiques/script.js
@@ -0,0 +1,9 @@
+/* La gestion des evenements en JavaScript se fait a l'aide d'event listeners.
+ * Un event listener est en realite une boucle qui, une fois le listener declare,
+ * est executee en permanence par le navigateur. Il peut etre applique a n'importe quel
+ * element d'une page, pour gerer les evenements localement, ou etre applique sur
+ * tout le document.*/
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
